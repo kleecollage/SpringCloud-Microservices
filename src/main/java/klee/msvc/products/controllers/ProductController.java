9f58d7229efffb66ffaef0f0748ee6f04c475193
Klee Collage <klee.collage@gmail.com>
2025-03-26 20:00:50 -0600

    refactor(controller): Create base url for api products

--- src/main/java/klee/msvc/products/controllers/ProductController.java
+++ src/main/java/klee/msvc/products/controllers/ProductController.java
@@ -5,12 +5,14 @@ import klee.msvc.products.services.IProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/products")
 public class ProductController {
 
     final private IProductService service;
