ccce4844f7f5b735db315283918b4b3346e3f21a
Klee Collage <klee.collage@gmail.com>
2025-03-27 17:08:58 -0600

    fix(serviceWebClient): Transform error 500 to 404 for products not found

--- src/main/java/klee/msvc/items/services/ItemServiceWebClient.java
+++ src/main/java/klee/msvc/items/services/ItemServiceWebClient.java
@@ -2,10 +2,10 @@ package klee.msvc.items.services;
 
 import klee.msvc.items.models.Item;
 import klee.msvc.items.models.ProductDto;
-import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.*;
 
@@ -35,14 +35,18 @@ public class ItemServiceWebClient implements IItemService {
     public Optional<Item> findById(long id) {
         Map<String, Long> params = new HashMap<>();
         params.put("id", id);
-        return Optional.ofNullable(client.build()
-                .get()
-                .uri("http://msvc-products/api/products/{id}", params)
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(ProductDto.class)
-                .map(product -> new Item(product, new Random().nextInt(10)+1))
-                .block());
+        try {
+            return Optional.ofNullable(client.build()
+                    .get()
+                    .uri("http://msvc-products/api/products/{id}", params)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToMono(ProductDto.class)
+                    .map(product -> new Item(product, new Random().nextInt(10)+1))
+                    .block());
+        } catch (WebClientResponseException e) {
+            return Optional.empty();
+        }
     }
 }
 
