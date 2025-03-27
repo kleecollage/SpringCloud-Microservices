a502c87d97bea0096e9ff49877e3507edcdd423d
Klee Collage <klee.collage@gmail.com>
2025-03-26 19:21:23 -0600

    feat(controllers): Implement Product controller with default map ('/') and '/{id}' to get all products and by id

---
+++ src/main/java/klee/msvc/products/controllers/ProductController.java
@@ -0,0 +1,34 @@
+package klee.msvc.products.controllers;
+
+import klee.msvc.products.entities.Product;
+import klee.msvc.products.services.IProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ProductController {
+
+    final private IProductService service;
+    public ProductController(IProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Product> list() {
+        return this.service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> get(@PathVariable long id) {
+        Optional<Product> productOptional = this.service.findById(id);
+        if (productOptional.isPresent()) {
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
