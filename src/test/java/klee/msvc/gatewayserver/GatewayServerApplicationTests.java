82f84d259b531af1d75a27d0a413f2a6d8b4ebf6
Klee Collage <klee.collage@gmail.com>
2025-03-27 18:58:43 -0600

    First commit

---
+++ src/test/java/klee/msvc/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package klee.msvc.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
