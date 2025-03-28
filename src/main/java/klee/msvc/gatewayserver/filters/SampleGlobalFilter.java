77841e14b416916b016b9fc86bb502bd8e5d11dc
Klee Collage <klee.collage@gmail.com>
2025-03-27 21:29:16 -0600

    feat(global filters): Implement pre request filters and post response filtes

--- src/main/java/klee/msvc/gatewayserver/filters/SampleGlobalFilter.java
+++ src/main/java/klee/msvc/gatewayserver/filters/SampleGlobalFilter.java
@@ -4,20 +4,55 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 @Component
-public class SampleGlobalFilter implements GlobalFilter {
+public class SampleGlobalFilter implements GlobalFilter, Ordered {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         logger.info("PRE: Executing filter before request");
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+        ServerHttpRequest mutatedRequest = exchange.getRequest()
+                .mutate()
+                .header("token", "Abc123") // Aquí se agrega el header
+                .build();
+        ServerWebExchange mutatedExchange = exchange.mutate()
+                .request(mutatedRequest)
+                .build();
+
+        return chain.filter(mutatedExchange).then(Mono.fromRunnable(() -> {
             logger.info("POST: Executing filter after response");
+            // TRADITIONAL WAY //
+            String token = mutatedExchange.getRequest().getHeaders().getFirst("token");
+            if (token != null) {
+                logger.info("token: {}", token);
+                exchange.getResponse().getHeaders().add("token", token);
+            }
+
+            // THE BEST WAY, THE REACTIVE WAY //
+            Optional.ofNullable(mutatedExchange.getRequest().getHeaders().getFirst("token"))
+                    .ifPresent(value -> {
+                        logger.info("token: {}", value);
+                        exchange.getResponse().getHeaders().add("token", value);
+                    });
+
+            exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "red").build());
+            exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
         }));
+    }
+
+    @Override
+    public int getOrder() {
+        return 100;
     }
 }
