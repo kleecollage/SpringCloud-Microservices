02a9ee1ff822f1e4e6cbe07c8cf472e69cf922da
Klee Collage <klee.collage@gmail.com>
2025-03-29 16:55:55 -0600

    First commit

---
+++ src/test/java/klee/msvc/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package klee.msvc.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
