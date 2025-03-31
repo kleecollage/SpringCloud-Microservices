2580f8a16b9af64149845b7cab6d036908fe55a2
Klee Collage <klee.collage@gmail.com>
2025-03-31 13:24:48 -0600

    First commit

---
+++ src/test/java/klee/msvc/oauth/OAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package klee.msvc.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
