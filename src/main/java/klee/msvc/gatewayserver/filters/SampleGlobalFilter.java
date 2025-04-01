3dbf2b2144b137ebea9ae4491563f3948fb336a1
Klee Collage <klee.collage@gmail.com>
2025-04-01 16:40:04 -0600

    fix(global filter): Implement filter chain to get code as response

--- src/main/java/klee/msvc/gatewayserver/filters/SampleGlobalFilter.java
+++ src/main/java/klee/msvc/gatewayserver/filters/SampleGlobalFilter.java
@@ -17,7 +17,7 @@ public class SampleGlobalFilter implements Filter, Ordered {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
-
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 }
 
