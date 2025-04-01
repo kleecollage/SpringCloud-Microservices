39cbfa529553526ee78fd73e0741d47b9d564e73
Klee Collage <klee.collage@gmail.com>
2025-04-01 14:45:03 -0600

    feat(token): Get roles from token claims to restrict paths

--- src/main/java/klee/msvc/gatewayserver/security/SecurityConfig.java
+++ src/main/java/klee/msvc/gatewayserver/security/SecurityConfig.java
@@ -2,10 +2,22 @@ package klee.msvc.gatewayserver.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,6 +31,11 @@ public class SecurityConfig {
                     .pathMatchers(HttpMethod.GET, "/api/items", "/api/products", "/api/users")
                     .permitAll()
 
+                    /* TO WORK WITH SCOPES READ/WRITE
+                    .pathMatchers(HttpMethod.GET, "/api/items/{id}", "/api/products/{id}", "api/users/{id}")
+                    .hasAnyAuthority("SCOPE_write", "SCOPE_read")*/
+
+                    // TO WORK WITH ROLES
                     .pathMatchers(HttpMethod.GET, "/api/items/{id}", "/api/products/{id}", "api/users/{id}")
                     .hasAnyRole("ADMIN", "USER")
 
@@ -29,9 +46,20 @@ public class SecurityConfig {
                     .pathMatchers(HttpMethod.DELETE, "/api/items/{id}", "/api/products/{id}", "/api/users/{id}").hasRole("ADMIN")*/
                     .anyExchange().authenticated();
         }).cors(ServerHttpSecurity.CorsSpec::disable)
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .oauth2Login(withDefaults())
                 .oauth2Client(withDefaults())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(new Converter<Jwt, Mono<AbstractAuthenticationToken>>() {
+                            @Override
+                            public Mono<AbstractAuthenticationToken> convert(Jwt source) {
+                                Collection<String> roles = source.getClaimAsStringList("roles");
+                                Collection<GrantedAuthority> authorities = roles.stream()
+                                        .map(SimpleGrantedAuthority::new)
+                                        .collect(Collectors.toList());
+                                return Mono.just(new JwtAuthenticationToken(source, authorities));
+                            }
+                        })))
                 .build();
     }
 }
