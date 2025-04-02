32b3c8d80b24dad12be217cfbfa3ffa69384daf7
Klee Collage <klee.collage@gmail.com>
2025-04-01 18:30:47 -0600

    docs(logs): Create logs in endpoints to trace

--- src/main/java/klee/msvc/users/controllers/UserController.java
+++ src/main/java/klee/msvc/users/controllers/UserController.java
@@ -3,6 +3,8 @@ package klee.msvc.users.controllers;
 import jakarta.validation.Valid;
 import klee.msvc.users.entities.User;
 import klee.msvc.users.services.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final IUserService userService;
 
     public UserController(IUserService userService) {
@@ -21,12 +23,14 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<Iterable<User>> findAll() {
+        logger.info("UserController::findAll. Fetching all users");
         Iterable<User> users = userService.findAll();
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
+        logger.info("UserController::findById. Getting user with id: {}", id);
         Optional<User> user = userService.findById(id);
         return user.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
@@ -34,6 +38,7 @@ public class UserController {
 
     @GetMapping("/username/{username}")
     public ResponseEntity<User> findByUsername(@PathVariable String username) {
+        logger.info("UserController::findByUsername. Login with username {}", username);
         Optional<User> user = userService.findByUsername(username);
         return user.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
@@ -41,6 +46,7 @@ public class UserController {
 
     @GetMapping("/email/{email}")
     public ResponseEntity<User> findByEmail(@PathVariable String email) {
+        logger.info("UserController::findByEmail. Login with email {}", email);
         Optional<User> user = userService.findByEmail(email);
         return user.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
@@ -48,6 +54,7 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> create(@Valid @RequestBody User user) {
+        logger.info("UserController::create: Creating {}", user);
         if (userService.existsByUsername(user.getUsername()))
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
 
@@ -60,6 +67,7 @@ public class UserController {
 
     @PutMapping("/{id}")
     public ResponseEntity<User> update(@PathVariable Long id, @Valid @RequestBody User user) {
+        logger.info("UserController::update: Updating {}", user);
         if (userService.findById(id).isEmpty())
             return ResponseEntity.notFound().build();
         user.setId(id);
@@ -69,6 +77,7 @@ public class UserController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
+        logger.info("UserController::delete: Deleting user with ID {}", id);
         if (userService.findById(id).isEmpty())
             return ResponseEntity.notFound().build();
 
