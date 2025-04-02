3b027b7f60385a39f36dc899baf83af14604b268
Klee Collage <klee.collage@gmail.com>
2025-04-01 19:28:57 -0600

    refactor(msvc-product): Move configuration to GatewayServerApplication in a programmatic way to avoid errors

--- src/main/java/klee/msvc/gatewayserver/GatewayServerApplication.java
+++ src/main/java/klee/msvc/gatewayserver/GatewayServerApplication.java
@@ -2,12 +2,35 @@ package klee.msvc.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
+import static org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions.circuitBreaker;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.stripPrefix;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> routerConfig() {
+        return route("msvc-products")
+                .route(path("/api/products/**"), http())
+                .before(stripPrefix(0))
+                .filter(lb("msvc-products"))
+                .filter(circuitBreaker(config -> config
+                        .setId("products")
+                        .setStatusCodes("500")
+                        .setFallbackUri("forward:/api/items/1")))
+                .build();
     }
 
 }
